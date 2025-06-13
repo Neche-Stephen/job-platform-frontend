20dc5db047b7aac4477620105c9bda3c9c4b0658
Chinecherem Ubawike <chinecheremubawike@gmail.com>
2025-06-13 19:33:06 +0100

    Add Post Job form page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About'
 import HowItWorks from './components/HowItWorks'
 import Benefits from './components/Benefits'
 import Footer from './components/Footer'
+import PostJobForm from './components/PostJobForm'
 
 export default function App() {
+  const [showPostJob, setShowPostJob] = useState(false)
+
   return (
     <div>
-      <Header />
-      <Hero />
-      <About />
-      <HowItWorks />
-      <Benefits />
-      <Footer />
+      <Header onPostJobClick={() => setShowPostJob(true)} />
+      {showPostJob ? (
+        <PostJobForm onBack={() => setShowPostJob(false)} />
+      ) : (
+        <>
+          <Hero />
+          <About />
+          <HowItWorks />
+          <Benefits />
+          <Footer />
+        </>
+      )}
     </div>
   )
 }
